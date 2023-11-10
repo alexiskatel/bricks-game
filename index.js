cf2994f3f8e7c8c9d0c9769f459ea47b64db3016
Alexis Katel <133600962+alexiskatel@users.noreply.github.com>
2023-11-10 08:40:38 +0100

    Update index.js1

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ const time = setInterval(() => {
     function author(){
         ctx.font = "15px Arial";
         ctx.fillStyle = "black";
-        ctx.fillText('Gagne, et envoie une capture au 63591464 pour avoir 1000F-----by Alexis Katel', canvas.width - 60, canvas.height - 5);
+        ctx.fillText('.Gagne, et envoie une capture au 63591464 pour avoir 1000F-----by Alexis Katel', canvas.width - 60, canvas.height - 5);
     }
 
     function draw() {
