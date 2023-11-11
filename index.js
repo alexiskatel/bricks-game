87f03f3feecc1d0e91892db3578385ca98380fbf
Alexis Katel <133600962+alexiskatel@users.noreply.github.com>
2023-11-11 14:49:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ function draw() {
     dy = -dy;
   } else if (y + dy > canvas.height - ballRadius) {
     if (x >= paddleX && x <= paddleX + paddleWidth) {
-      dy = -dy - (Math.random() * 50 + 80) / 100;
+      dy = -dy - (Math.random() * 50 + 70) / 100;
     } else {
       gameOver();
       clearInterval(interval);
